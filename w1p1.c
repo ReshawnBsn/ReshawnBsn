080580a9a68083d8c538e01a2f329ec156858837
ReshawnBsn <144815836+ReshawnBsn@users.noreply.github.com>
2023-09-12 10:56:22 -0400

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,40 @@
+/*****************************************************************************
+<assessment name example: Workshop - #1 (Part-1)>
+Full Name : Reshawn Walcott
+Student ID#:163607197
+Email :rwalcott1@myseneca.ca
+Section :NKK
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+ 
+    #include <stdio.h>
+
+int main(void) 
+{
+    printf ("Workshop 1 Part-1\n");
+    printf ("=================\n");
+        printf ("\n");
+    printf ("I'm displaying this using the 'printf' stdio\n");
+    printf ("(standard input output) library function!\n");
+        printf ("\n");
+    printf ("Dear teacher,");
+        printf ("\n");
+        printf ("\n");
+    printf ("  I promise I will work hard from this day onward.\n");
+    printf ("  I acknowledge that practice is extremely important,\n");
+    printf ("  so I will do all workshops, quizzes, and assignments.\n");
+        printf ("\n");
+    printf ("Sincerely,\n");
+        printf ("\n");
+    printf ("Reshawn Walcott\n");
+    printf ("Student ID# 163607197\n");
+
+    
+
+    
+    return 0;
+}
